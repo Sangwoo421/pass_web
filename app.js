21f51bf96aa46d0041936cee69acbf4c00ea6d7d
sangsu1 <nss2882@naver.com>
2023-11-11 11:33:04 +0900

    qusrud

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const app = express();
 
 const PORT = 8080;
 app.listen(PORT, function() {
-<<<<<<< HEAD
-    console.log('존잘상우');
-=======
-    console.log('나는 상수');
->>>>>>> 0908eb792d43536998c8c00b880d6152cf772ef9
+    console.log('존aht우');
     console.log('Listening on port: ', PORT);
 });
